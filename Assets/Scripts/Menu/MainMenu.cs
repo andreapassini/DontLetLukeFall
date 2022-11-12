a18f671adf1314e386e9212160878c124f832b33
Carlo Ambrogi <carlomaria.ambrogi@icloud.com>
2022-11-12 18:09:15 +0100

    Started to create main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _buttonToFirstSelect;
+    [SerializeField] private EventSystem _eventSystem;
+
+    void Update()
+    {
+        if (Input.anyKeyDown && !(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)))
+        {
+            if (!_eventSystem.currentSelectedGameObject)
+            {
+                _eventSystem.SetSelectedGameObject(_buttonToFirstSelect);
+            }
+        }
+    }
+    
+}
