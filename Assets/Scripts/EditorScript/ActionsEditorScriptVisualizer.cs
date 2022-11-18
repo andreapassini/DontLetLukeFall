0c872db473debf4883c09756b43ef764356aac37
Carlo Ambrogi <carlomaria.ambrogi@icloud.com>
2022-11-18 15:48:22 +0100

    set starting position of first image for editor script

--- Assets/Scripts/EditorScript/ActionsEditorScriptVisualizer.cs
+++ Assets/Scripts/EditorScript/ActionsEditorScriptVisualizer.cs
@@ -14,21 +14,20 @@ public class ActionsEditorScriptVisualizer : MonoBehaviour
 
     private const float _referenceCameraSize = 8.457275f;
     private const float _referenceImageMeasure = 321f;
+    private const float _adapterCameraCanvas = 0.0005f;
     
     private void Awake()
     {
+        // fixing image dimensions
         float measure = _referenceCameraSize * _referenceImageMeasure / _camera.orthographicSize;
         ((RectTransform)_image.transform).sizeDelta = new Vector2 (measure, measure);
         // Now I have fixed the image dimensions
-        // Now I fix the image position to Luke's position
-        _image.transform.position = new Vector3(0,0,0);
-        Debug.Log("rrr" + _canvas.pixelRect.width);
-        float canvasWidth = _canvas.pixelRect.width;
-        float canvasHeight = _canvas.pixelRect.height;
-        float cameraWidth = _camera.sensorSize.x;
-        float cameraHeight = _camera.sensorSize.y;
-        Debug.Log("rrrrrr" + cameraWidth);
+        // fixing image position to Luke's position
         Vector3 relativePositionOfLukeToCamera = _camera.transform.InverseTransformDirection(_luke.transform.position - _camera.transform.position);
+        float multiplier = 952.0f / 15.0f;
+        multiplier = multiplier * _referenceCameraSize / _camera.orthographicSize;
+        multiplier = multiplier * _canvas.pixelRect.width / 1920f;
+        _image.transform.position = new Vector3(multiplier * relativePositionOfLukeToCamera.x + _canvas.pixelRect.width / 2,multiplier * relativePositionOfLukeToCamera.y + _canvas.pixelRect.height / 2,_image.transform.position.z);
     }
-    
+
 }
