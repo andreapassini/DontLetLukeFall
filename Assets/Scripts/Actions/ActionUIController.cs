dcdd1e78eb216f941d7cb51288d6cc9946f08dac
Carlo Ambrogi <carlomaria.ambrogi@icloud.com>
2022-12-15 09:50:21 +0100

    Fixed images for action UI

--- Assets/Scripts/Actions/ActionUIController.cs
+++ Assets/Scripts/Actions/ActionUIController.cs
@@ -7,15 +7,11 @@ using UnityEngine.UI;
 public class ActionUIController : MonoBehaviour
 {
 
-    [SerializeField] private int _numActionsToDisplay; // This is to set the number of images
-    [SerializeField] private GameObject _canvas; // This is the canvas where to visualize images
-    [SerializeField] private Image _image; // This is the image to be cloned; this image should be located at the center of the canvas
     [SerializeField] private Sprite _transparentSprite; // This is a transparent sprite used when there are less actions then the number of actions to display
-    [SerializeField] private bool _avoidDoubleOffset; // Avoid double offset when showing images of actions in action ui
-    
-    private Image[] _displayActionImages; // this array is initialized with 5 elements (Image) via inspector
+    [SerializeField] private Image[] _displayActionImages; // this array is setted with images via inspector
     // Each Image represent an action
-    private Image _imageBar; // this is the image with the bar witch has an animation to show the current action
+    [SerializeField] private Image _imageBar; // this is the image with the bar witch has an animation to show the current action
+    
     private List<Sprite> _actionsSprites; // In this list it will memorized the list of sprite of actions using method LoadActionSequence
     private const float TimeToCompleteAnimation = 0.5f;
 
@@ -23,39 +19,14 @@ public class ActionUIController : MonoBehaviour
     private bool _hasBeenLoaded;
 
     private void Awake()
-    // At the beginning images are created; these images go to be located where there was the image to be cloned (at the center of the canvas)
     {
-        float imageLenght = _image.rectTransform.rect.width;
-        float movingPosFirstImage = (imageLenght * (_numActionsToDisplay - 1));
-        _image.rectTransform.position = new Vector3(_image.rectTransform.position.x - movingPosFirstImage,
-            _image.rectTransform.position.y, _image.rectTransform.position.z);
-        if (_avoidDoubleOffset == false)
+        foreach (var el in _displayActionImages) // starting fixings
         {
-            _image.rectTransform.position = new Vector3(_image.rectTransform.position.x - movingPosFirstImage,
-                _image.rectTransform.position.y, _image.rectTransform.position.z);
+            el.sprite = _transparentSprite;
+            el.GetComponent<Animator>().enabled = false;
         }
-        // The image to be cloned is moved to the fist position
-        _displayActionImages = new Image[_numActionsToDisplay];
-        for (int i = 0; i < _numActionsToDisplay; i++)
-        {
-            // For each iteration the image to be cloned is cloned and then moved to the next position
-            Image newImage = Instantiate(_image, _image.transform.position, _image.transform.rotation, gameObject.transform);
-            _displayActionImages[i] = newImage;
-            newImage.sprite = _transparentSprite;
-            newImage.GetComponent<Animator>().enabled = false;
-            _image.rectTransform.position = new Vector3(_image.rectTransform.position.x + imageLenght,
-                _image.rectTransform.position.y, _image.rectTransform.position.z);
-            if (_avoidDoubleOffset == false)
-            {
-                _image.rectTransform.position = new Vector3(_image.rectTransform.position.x + imageLenght,
-                    _image.rectTransform.position.y, _image.rectTransform.position.z);
-            }
-        }
-        _imageBar = Instantiate(_image, _image.transform.position, _image.transform.rotation, gameObject.transform); // This is the image to show the bar of the action
-        _image.enabled = false; // The original image to be cloned is disabled
         _imageBarAnimator = _imageBar.GetComponent<Animator>();
         _imageBarAnimator.enabled = false; // At the beginning the bar is disabled and it is enabled when it's called LoadActionSequence
-        _imageBar.transform.position = _displayActionImages[_displayActionImages.Length-1].transform.position; // The bar will be in the same position of the last image
     }
 
     public int GetNumberOfDisplayedActions()
