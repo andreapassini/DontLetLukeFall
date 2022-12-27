b4b5758f16d18450713a2a91a1308cacf831df81
Carlo Ambrogi <carlomaria.ambrogi@icloud.com>
2022-12-27 16:39:26 +0100

    Implemented with Feel feedback

--- Assets/Scripts/AnimationPlacingPlatform.cs
+++ Assets/Scripts/AnimationPlacingPlatform.cs
@@ -1,38 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 
 public class AnimationPlacingPlatform : MonoBehaviour
 {
-    
-    private float _timer = 0.0f;
-    private bool _needToExecuteAnimation = false;
-    private float _startScaleValue = 2.0f;
-    private float _speed = 2.25f;
 
-    void Update()
-    {
-        if (_needToExecuteAnimation)
-        {
-            _timer += (Time.deltaTime * _speed);
-            float val = _startScaleValue - _timer;
-            if (val < 1.0f)
-            {
-                gameObject.transform.localScale = new Vector3(1, 1, 1);
-                _needToExecuteAnimation = false;
-            }
-            else
-            {
-                gameObject.transform.localScale = new Vector3(val, val, val);
-            }
-        }
-    }
+    [SerializeField] private MMFeedbacks _feedbackAnimationPlacingPlatform;
 
     public void ExecuteAnimation()
     {
-        gameObject.transform.localScale = new Vector3(_startScaleValue, _startScaleValue, _startScaleValue);
-        _timer = 0.0f;
-        _needToExecuteAnimation = true;
+        _feedbackAnimationPlacingPlatform?.PlayFeedbacks();
     }
     
 }
