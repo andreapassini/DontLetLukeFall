394f92b5ef411c427f00f61a8376b9f096948c45
Andrea Passini <71270277+andreapassini@users.noreply.github.com>
2023-01-02 12:17:20 +0100

    Intro reworked (#80)
    * Scene Intro with new sprites
    
    * Update Tutorial.unity
    
    * Update EditorBuildSettings.asset

--- Assets/Scripts/Video/LukeFalling.cs
+++ Assets/Scripts/Video/LukeFalling.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public class LukeFalling : MonoBehaviour
 {
     [SerializeField] private float _dealy;
+    [SerializeField] private float _dealyFeedbackSound;
     private Rigidbody2D _rb2D;
     [SerializeField] private float _gravityScale;
     private AudioSource _audioSource;
+    private MMF_Player _fallingFeedback;
 
     void Start()
     {
         _rb2D ??= transform.GetComponent<Rigidbody2D>();
-        _audioSource ??= transform.GetComponent<AudioSource>();
+        _fallingFeedback = GetComponent<MMF_Player>();
 
         _rb2D.gravityScale = 0f;
-        _audioSource.PlayDelayed(_dealy);
 
         StartCoroutine(DelayFall());
+        StartCoroutine(DealyFeedback());
     }
 
     private IEnumerator DelayFall()
@@ -25,5 +28,13 @@ public class LukeFalling : MonoBehaviour
         yield return new WaitForSeconds(_dealy);
 
         _rb2D.gravityScale = _gravityScale;
+    }
+
+    private IEnumerator DealyFeedback()
+    {
+        yield return new WaitForSeconds(_dealyFeedbackSound);
+
+        _fallingFeedback ??= GetComponent<MMF_Player>();
+        _fallingFeedback.PlayFeedbacks();
     }
 }
