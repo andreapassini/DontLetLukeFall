2d3c8fbe3a52123248e7d30f4eb0cbbe52e0f6dc
LucaFin <luca.finoia@gmail.com>
2023-01-05 17:10:54 +0100

    Created Level 9

--- Assets/Scripts/Platforms/LerpPath.cs
+++ Assets/Scripts/Platforms/LerpPath.cs
@@ -5,31 +5,44 @@ using UnityEngine;
 public class LerpPath : MonoBehaviour
 {
     [SerializeField]
-    private List<Vector2> Path;
-    private float time = 0;
-    private int actualTarget=0;
-    private int nextTarget=1;
-    private Vector2 offset;
+    private List<Vector2> _path;
+    private float _time = 0;
+    private int _actualTarget=0;
+    private int _nextTarget=1;
+    private Vector2 _offset;
     [SerializeField]
-    private float speed = 0.25f;
+    private float _speed = 0.25f;
+    [SerializeField]
+    private float _waitingTime = 0;
+    private float _timer = 0;
     private void Awake()
     {
-        offset = transform.position - (Vector3)Path[0];
+        _timer = _waitingTime;
+        _offset = transform.position - (Vector3)_path[0];
     }
     // Update is called once per frame
     void Update()
     {
 
-        time += Time.deltaTime * speed * speed / Vector2.Distance(Path[actualTarget],Path[nextTarget]);
-        if (time < 1)
+        _time += Time.deltaTime * _speed * _speed / Vector2.Distance(_path[_actualTarget],_path[_nextTarget]);
+        if (_time < 1)
         {
-            transform.position = Vector3.Lerp(Path[actualTarget] + offset, Path[nextTarget] + offset, time);
+            transform.position = Vector3.Lerp(_path[_actualTarget] + _offset, _path[_nextTarget] + _offset, _time);
         }
         else
         {
-            time = 0;
-            actualTarget = actualTarget != Path.Count-1 ? actualTarget + 1 : 0;
-            nextTarget = nextTarget != Path.Count-1 ? nextTarget + 1 : 0;
+            if (_waitingTime <= 0)
+            {
+                _waitingTime = _timer;
+                _time = 0;
+                _actualTarget = _actualTarget != _path.Count - 1 ? _actualTarget + 1 : 0;
+                _nextTarget = _nextTarget != _path.Count - 1 ? _nextTarget + 1 : 0;
+            }
+            else
+            {
+                _waitingTime -= Time.deltaTime;
+                Debug.Log(_waitingTime);
+            }
         }
         
     }
